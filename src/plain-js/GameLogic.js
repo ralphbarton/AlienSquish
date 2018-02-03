0205b6931ed081b760e87f3a823ece2da72c8e7b
Ralph Barton <ralph_barton_@hotmail.com>
2018-02-03 15:32:14 +0000

    further improvements involving the boulder-push function: simplified code  - there is no need to actually separately check if the player is moving into boulders at all, or if movement is offscreen. This work can all be offloaded into the recursive 'nudge' function. Very neat!

--- src/plain-js/GameLogic.js
+++ src/plain-js/GameLogic.js
@@ -67,70 +67,48 @@ const GameLogic = {
     playerMove: function(state, ArrowKey){
 	// ArrowKey = { d=[ 'x' | 'y' ], v = [ +1 -1 ] }
 
-	// 1. check if at edge of board (means no action)
-	const max = state.board[ArrowKey.d === "x" ? "width" : "height"] - 1;
-	if(ArrowKey.v<0){// wants to move up or left
-	    if(state.player[ArrowKey.d] <= 0){console.log("edge");return state;}//no action if at adge
-	}else{// wants to move down or right
-	    if(state.player[ArrowKey.d] >= max){console.log("edge");return state;}//no action if at adge
-	}
+	const isX = ArrowKey.d === 'x'; // moving along x-direction
+	const max = state.board[isX ? "width" : "height"] - 1;
+	var latestState = state;
+	
+	function getNext(Coords){
+	    return {
+		x: Coords.x + (isX ? ArrowKey.v : 0),
+		y: Coords.y + (isX ? 0 : ArrowKey.v)
+	    };
+	};
+	
+	function nudgeInto(Coords, nudger){
 
-	const playerNext = {
-	    x: state.player.x + (ArrowKey.d === "x" ? ArrowKey.v : 0),
-	    y: state.player.y + (ArrowKey.d === "y" ? ArrowKey.v : 0)
+	    // case 1: nudge is into a location which is outside the board
+	    const xy = isX ? Coords.x : Coords.y; //whichever of x or y is the direction of pushing.
+	    if((xy < 0) || (xy > max)){return false;} // no movement
+	    
+	    // cases 2 & 3: where nudge is into an empty space (2) or a boulder (3)
+	    const intoCell = latestState.board.cells[Coords.y][Coords.x];
+	    const Coords2 = getNext(Coords);
+
+	    /* this condition (incorporating the recursion) reads as does the motion
+	       actually take place (potentially some way 'downstream'...) */
+	    if(!intoCell || nudgeInto(Coords2, intoCell)){
+
+		latestState = update(latestState, {
+		    board: {
+			cells: {[Coords.y]: {[Coords.x]:  {$set: nudger}  }}
+		    }
+		});
+		return true;
+	    }
 	};
 
-	const cellEntered = state.board.cells[playerNext.y][playerNext.x];
-	
-	if(!cellEntered){//simply moving into empty space
-
-	    return update(state, {
-		player: {$set: playerNext}
-	    });
-
+	// passing the 'null' will leave an empty space where the pushed bolder moves from
+	const playerNext = getNext(state.player);
+	if(nudgeInto(playerNext, null)){
+	    //movement occurred. Let player move
+	    return update(latestState, {player: {$set: playerNext}});
 	}else{
-	    // for now, assume moving into a Rock
-
-	    const isX = ArrowKey.d === 'x'; // moving along x-direction
-	    
-	    var latestState = state;
-
-	    function nudgeInto(x, y, nudger){
-
-		// case 1: nudge is into a location which is outside the board
-		const xy = isX ? x : y; //whichever of x or y is the direction of pushing.
-		if((xy < 0) || (xy > max)){return false;} // no movement
-	
-		
-		// cases 2 & 3: where nudge is into an empty space (2) or a boulder (3)
-		const intoCell = latestState.board.cells[y][x];
-		const x_ = x + (isX ? ArrowKey.v : 0);
-		const y_ = y + (isX ? 0 : ArrowKey.v);
-
-		/* this condition (incorporating the recursion) reads as does the motion
-		   actually take place (potentially some way 'downstream'...) */
-		if(!intoCell || nudgeInto(x_, y_, intoCell)){
-
-		    latestState = update(latestState, {
-			board: {
-			    cells: {[y]: {[x]:  {$set: nudger}  }}
-			}
-		    });
-		    return true;
-		}
-		
-	    };
-
-	    // paasing null will leave an empty space where the pushed bolder moves from
-	    if(nudgeInto(playerNext.x, playerNext.y, null)){
-		//movement occurred. Let player move
-		return update(latestState, {player: {$set: playerNext}});
-	    }else{
-		// those boulders aren't moving!
-		return state;
-	    }
-	    
-	    
+	    // those boulders aren't moving!
+	    return state;
 	}
 	
     }
